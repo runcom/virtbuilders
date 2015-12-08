e1643043222dce7f4bd8670506c48ee38614a2aa
Antonio Murdaca <runcom@redhat.com>
2015-12-08 12:36:41 +0100

    For real, this time.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 case $1 in
 	rawhide)
 		name=${OSNAME:-rawhide}
+		root_password=${PASSWORD:-rawhide}
 		dist="fedora-23"
 		# using fedora22 as os-variant becuse virt-install ins't updatd yet probably and errors out
 		osvariant="fedora22"
@@ -15,5 +16,5 @@ case $1 in
 		;;
 esac
 
-virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$os --update $options --size 10G $run
+virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$root_password --update $options --size 10G $run
 virt-install --name $name --ram 1024 --disk path=$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --import
