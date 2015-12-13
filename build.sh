c2243703e6b5520a95d4843638f3625e29e99f03
Antonio Murdaca <runcom@redhat.com>
2015-12-13 11:00:47 +0100

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
+is_available() {
+	available=( $(virsh list --all --name) )
+	for i in "${available[@]}"
+	do
+	    if [ "$i" == "$1"  ] ; then
+		    echo "domain $1 already imported, please destroy and undefine it first"
+		    exit
+	    fi
+	done
+}
+
+check_root() {
+	if [ -e ./$1.qcow2 ]; then
+		echo "$1.qcow2 exists, please remove it"
+		exit
+	fi
+}
+
 case $1 in
 	rawhide)
+		is_available rawhide
+		check_root rawhide
 		name=${OSNAME:-rawhide}
 		root_password=${PASSWORD:-rawhide}
 		dist="fedora-23"
@@ -9,6 +29,7 @@ case $1 in
 		osvariant="fedora22"
 		run="$RUN --run rawhide/torawhide.sh"
 		options="$OPTS --selinux-relabel"
+		hostname=${HOSTNAME:-rawhide}
 		;;
 	*)
 		echo "os not supported"
@@ -16,5 +37,5 @@ case $1 in
 		;;
 esac
 
-virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$root_password --update $options --size 10G $run
-virt-install --name $name --ram 1024 --disk path=$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --import
+virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$root_password --update $options --size 10G $run --hostname $hostname
+virt-install --name $name --ram 2048 --vcpus=2 --network bridge=virbr0 --disk path=$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --import
--- rawhide/torawhide.sh
+++ rawhide/torawhide.sh
@@ -2,4 +2,27 @@ dnf install -y fedora-repos-rawhide
 dnf config-manager --set-disabled fedora updates updates-testing
 dnf config-manager --set-enabled rawhide
 dnf --releasever=rawhide distro-sync -y --nogpgcheck
+
+# installing stuff I need while hacking on docker
+dnf update -y
+dnf install -y docker golang git glibc-static systemd-devel device-mapper-devel audit-libs-devel
+systemctl enable docker
+git clone https://github.com/runcom/docker /root/docker
+cd /root/docker \
+	&& git remote add upstream https://github.com/docker/docker \
+	&& git remote add projectatomic https://github.com/projectatomic/docker \
+	&& git fetch upstream -a --tags \
+	&& git fetch projectatomic -a --tags
+sh -c 'cat >/root/build-docker.sh <<-EOF
+#!/bin/sh
+
+if [ ! -e MAINTAINERS ]; then
+	echo "please run this script relative to the docker git directory, e.g. cd docker && ../build-docker.sh"
+	exit
+fi
+
+AUTO_GOPATH=1 BUILDFLAGS="-race" DOCKER_BUILDTAGS="experimental selinux journald exclude_graphdriver_btrfs exclude_graphdriver_zfs exclude_graphdriver_aufs" ./hack/make.sh dynbinary
+EOF'
+chmod +x /root/build-docker.sh
+
 reboot
