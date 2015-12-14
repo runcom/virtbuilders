0cdbf6946d0d3859d5906153512c1f6575e63545
Antonio Murdaca <runcom@redhat.com>
2015-12-14 14:29:44 +0100

    a few bits tried to escape, but we caught them

--- debian/totesting.sh
+++ debian/totesting.sh
@@ -1,6 +1,6 @@
 # XXX: have a look here to boot up the vm if it's stuck at "loading initial ramdisk" https://unix.stackexchange.com/questions/203768/debian-8-kvm-guest-loading-initial-ramdisk
 sed -i 's/jessie/testing/g' /etc/apt/sources.list
-DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
+apt-get -y update && APT_LISTCHANGES_FRONTEND=none apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
 apt-get autoremove && apt-get clean
 apt-get -y install vim
 systemctl enable getty@ttyS0
