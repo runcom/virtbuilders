0c0f81c15177bf674fe6eeb3fc572b18d2a85411
Antonio Murdaca <runcom@redhat.com>
2015-12-14 15:45:04 +0100

    put code that worked where the code that didn't used to be

--- debian/totesting.sh
+++ debian/totesting.sh
@@ -2,6 +2,14 @@
 sed -i 's/jessie/testing/g' /etc/apt/sources.list
 apt-get -y update && APT_LISTCHANGES_FRONTEND=none apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
 apt-get -y autoremove && apt-get -y clean
-apt-get -y install vim
+apt-get -y install vim apt-transport-https
 systemctl enable getty@ttyS0
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sh -c 'cat >/etc/apt/sources.list.d/docker.list <<-EOF
+deb https://apt.dockerproject.org/repo debian-stretch main
+EOF'
+apt-get -y update && apt-get -y install docker-engine
+systemctl enable docker
+
 reboot
