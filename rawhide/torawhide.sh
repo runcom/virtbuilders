1197f19e1dca4b517a726dbc381013257d5609c1
Antonio Murdaca <runcom@redhat.com>
2016-02-28 15:08:22 +0100

    a few bits tried to escape, but we caught them

--- rawhide/torawhide.sh
+++ rawhide/torawhide.sh
@@ -2,3 +2,4 @@ dnf install -y fedora-repos-rawhide
 dnf config-manager --set-disabled fedora updates updates-testing
 dnf config-manager --set-enabled rawhide
 dnf --releasever=rawhide distro-sync -y --nogpgcheck
+dnf install fedora-packager
