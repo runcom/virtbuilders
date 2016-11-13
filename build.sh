37ecc0354a6f9a5c1149718593c90530e48152fb
Antonio Murdaca <runcom@redhat.com>
2016-11-13 14:35:21 +0100

    add disk by default
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ is_available() {
 }
 
 check_root() {
-	if [ -e ./$1.qcow2 ]; then
-		echo "$1.qcow2 exists, please remove it"
+	if [ -e ./$1/$1.qcow2 ]; then
+		echo "$1/$1.qcow2 exists, please remove it"
 		exit
 	fi
 }
@@ -26,50 +26,50 @@ version=$2
 root_password=${PASSWORD:-$1}
 
 case $os in
-	ubuntu)
-		size=${SIZE:-32G}
-		is_available $name
-		check_root $name
-		ver=${UBUNTUVER:-14.04}
-		dist="ubuntu-$ver"
-		# TODO: ubuntu16.04 doesn't exist into osinfo-query os for instance and doesn't start uff
-		if [ "$ver" != "14.04" ]; then
-			ver="15.10"
-		fi
-		osvariant="ubuntu$ver"
-		;;
-	fedora)
-		case $version in
-			23)
-				size=${SIZE:-32G}
-				is_available $name
-				check_root $name
-				dist="fedora-23"
-				# using fedora22 as os-variant becuse virt-install ins't updatd yet probably and errors out
-				run="$RUN --run basic_fedora.sh"
-				osvariant="fedora22"
-				options="$OPTS --selinux-relabel"
-				;;
-			rawhide)
-				size=${SIZE:-32G}
-				is_available $name
-				check_root $name
-				dist="fedora-23"
-				# using fedora22 as os-variant becuse virt-install ins't updated yet probably and errors out
-				osvariant="fedora22"
-				run="$RUN --run basic_fedora.sh"
-				run="$run --run rawhide/torawhide.sh"
-				if [ -n "$STUFF" ]; then
-					run="$run --run rawhide/stuff.sh"
-				fi
-				options="$OPTS --selinux-relabel"
-				;;
-			*)
-				echo "version not supported"
-				exit
-				;;
-		esac
-		;;
+	#ubuntu)
+		#size=${SIZE:-32G}
+		#is_available $name
+		#check_root $name
+		#ver=${UBUNTUVER:-14.04}
+		#dist="ubuntu-$ver"
+		## TODO: ubuntu16.04 doesn't exist into osinfo-query os for instance and doesn't start uff
+		#if [ "$ver" != "14.04" ]; then
+			#ver="15.10"
+		#fi
+		#osvariant="ubuntu$ver"
+		#;;
+	#fedora)
+		#case $version in
+			#23)
+				#size=${SIZE:-32G}
+				#is_available $name
+				#check_root $name
+				#dist="fedora-23"
+				## using fedora22 as os-variant becuse virt-install ins't updatd yet probably and errors out
+				#run="$RUN --run basic_fedora.sh"
+				#osvariant="fedora22"
+				#options="$OPTS --selinux-relabel"
+				#;;
+			#rawhide)
+				#size=${SIZE:-32G}
+				#is_available $name
+				#check_root $name
+				#dist="fedora-23"
+				## using fedora22 as os-variant becuse virt-install ins't updated yet probably and errors out
+				#osvariant="fedora22"
+				#run="$RUN --run basic_fedora.sh"
+				#run="$run --run rawhide/torawhide.sh"
+				#if [ -n "$STUFF" ]; then
+					#run="$run --run rawhide/stuff.sh"
+				#fi
+				#options="$OPTS --selinux-relabel"
+				#;;
+			#*)
+				#echo "version not supported"
+				#exit
+				#;;
+		#esac
+		#;;
 	rhel|rawhide|centos|"rhel-atomic"|"centos-atomic"|"fedora-atomic")
 		path=$2
 		if [ -z $path ]; then
@@ -97,6 +97,10 @@ case $os in
 		esac
 		./gen_iso.sh $1 $name
 		sudo virt-install --name $name --ram 2048 --vcpus=2 --disk path=./$name/$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --disk path=./$name/init.iso,device=cdrom,readonly=on --import --noreboot
+		# add disk, default 4G, remember to format it :)
+		qemu-img create -f raw "./$name/$name.disk" 4G
+		sudo virsh attach-disk $name --source "$(pwd)/$name/$name.disk" --target vdb --persistent
+		# start it!
 		sudo virsh start $name
 		exit
 		;;
@@ -107,9 +111,10 @@ case $os in
 esac
 
 # libguestfs-xfs.x86_64 is needed for --size on my f23
-sudo virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$root_password $options --size $size $run --hostname $name
+#sudo virt-builder $dist -o $name.qcow2 --format qcow2 --root-password password:$root_password $options --size $size $run --hostname $name
 
-sudo virt-install --name $name --ram 2048 --vcpus=2 --disk path=$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --import
+#sudo virt-install --name $name --ram 2048 --vcpus=2 --disk path=$name.qcow2,format=qcow2,cache=writeback --nographics --os-variant $osvariant --import
+
 
 # subscribe:
 # subscription-manager register --username amurdaca@redhat.com --auto-attach
